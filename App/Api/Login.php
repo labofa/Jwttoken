65c5a03d5c0e5f3ba65b2f6b9739aeb8d4c13221
Ekpebu Woyenghilabofa <imiebiekpebu@yahoo.com>
2019-03-15 00:24:40 +0100

    Update Login.php

--- App/Api/Login.php
+++ App/Api/Login.php
@@ -4,7 +4,7 @@
 	
 	header("Access-Control-Allow-Origin: *");
 	header("Content-Type: application/json; charset=UTF-8");
-	header("Access-Control-Allow-Methods: Post");
+	header("Access-Control-Allow-Methods: GET");
 
 	#include connection page and objects
 	require '../conn.php';
